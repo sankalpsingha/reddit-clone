93f381f39e133b013d016c05a89a724aba17b4bf
sankalpsingha <sankalp.singha@autobahn-security.com>
2023-09-24 14:43:36 +0700

    Initial commit after the navbar basic

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demo learning project clone of Reddit.
+
+This is the demo learning project clone of Reddit.
+https://www.youtube.com/watch?v=mSUKMfmLAt0
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,34 @@
-import '@/styles/globals.css'
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+// Setting up a custom font for the application
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 antialiased bg-slate-50">
+        <Navbar />
+        <div className="container h-full py-12 mx-auto max-w-7xl">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
